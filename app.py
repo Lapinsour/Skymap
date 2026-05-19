f17fc868bae00329ba8d094e51474f4b0a3fc8a1
Pierre Garrigues <141630659+Lapinsour@users.noreply.github.com>
2026-05-19 09:42:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -231,8 +231,7 @@ elif page == "Bibliothèque":
         path = card["image"].lstrip("/")  # sécurité
 
         img_url = supabase.storage.from_("Skyline").get_public_url(card["image"]).replace("/cards","")
-        st.write(img_url)
-        st.write(card["image"])
+        
         with cols[i % 4]:
 
             st.markdown(f"""
